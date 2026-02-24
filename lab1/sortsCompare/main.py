b3bc875ad54b6f67282757ce2e7a44052133a735
maximus <mkorobchenko21@gmail.com>
2026-02-24 11:48:18 +0200

    refactored code; statically typed funcrions; improved code readability

--- lab1/sortsCompare/main.py
+++ lab1/sortsCompare/main.py
@@ -1,34 +1,36 @@
-import numpy as np
 import random
-from plot_data import plot_data
+from plotData2 import plot_data
 import math
 
 
-def genArr(size, genType = "random"):
+def genArr(size: int, genType: str = "random") -> list:
     match genType:
-        case 'best':
-            return [i for i in range(1, size)]
+        case "best":
+            return [i for i in range(1, size + 1)]
         case "worst":
-            return [i for i in reversed(range(1, size))]
+            return [i for i in reversed(range(1, size + 1))]
         case "random":
-            return [random.randint(1, size) for _ in range(1, size)]
+            return [random.randint(1, size) for _ in range(1, size + 1)]
         case "randomUniq":
-            return random.shuffle([i for i in range(1, size)])
+            arr = [i for i in range(1, size + 1)]
+            random.shuffle(arr)
+            return arr
 
-def bubbleSort(arr):
+
+def bubbleSort(arr: list) -> int:
     size = len(arr)
     operationCount = 0
- 
+
     for i in range(size - 1):
         for j in range(size - i - 1):
             operationCount += 1
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
     return operationCount
 
 
-def bubbleImprovedSort(arr):
+def bubbleImprovedSort(arr: list) -> int:
     size = len(arr)
     operationCount = 0
 
@@ -36,24 +38,26 @@ def bubbleImprovedSort(arr):
         swapped = False
         for j in range(size - i - 1):
             operationCount += 1
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 swapped = True
-        if swapped == False:
+        if not swapped:
             break
 
     return operationCount
 
 
-def shellSort(arr): # using Tokuda gap
-    def getTokudaGaps(n):
-            gaps = []
-            k = 1
-            while (gap := math.ceil((9**k - 4**k) / (5 * 4**(k - 1))) ) <= n:       # while this new counted gap <= n
-                gaps.append(gap)
-                k += 1
+def shellSort(arr: list) -> int:  # using Tokuda gap
+    def getTokudaGaps(n: int) -> list:
+        gaps = []
+        k = 1
+        while (
+            gap := math.ceil((9**k - 4**k) / (5 * 4 ** (k - 1)))
+        ) <= n:  # while this new counted gap <= n
+            gaps.append(gap)
+            k += 1
 
-            return reversed(gaps)
+        return reversed(gaps)
 
     size = len(arr)
     operationCount = 0
@@ -69,52 +73,58 @@ def shellSort(arr): # using Tokuda gap
                 arr[j] = arr[j - gap]
                 j -= gap
 
-            if j >= gap: # when in while loop was comparison, but it didnt counted
+            if j >= gap:  # when in while loop was comparison, but it didnt counted
                 operationCount += 1
 
             arr[j] = temp
 
     return operationCount
 
+
 if __name__ == "__main__":
-    """
-    ТЕСТУВАННЯ АЛГОРИТМІВ:
-    тестування проводиться на наборах розімром 10, 100, 1000, 10000 (масив sizes)
-    але для початкової перевірки роботи рекомендується використовувати тільки
-    перші дві-три величини
-    """
+    #
+    # TESTS
+    #
 
     sizes = [10, 100, 1000]
-    types = ["random", "best", "worst"]
+    typesArrayTest = ["random", "randomUniq", "best", "worst"]
 
-    data_plot = {
-        'random': {'bubble': {}, 'insertion': {}, 'bubble_impr': {}},
-        'best': {'bubble': {}, 'insertion': {}, 'bubble_impr': {}},
-        'worst': {'bubble': {}, 'insertion': {}, 'bubble_impr': {}}
+    dataPlot = {
+        "random": {"bubble": {}, "bubbleImproved": {}, "shell": {}},
+        "randomUniq": {"bubble": {}, "bubbleImproved": {}, "shell": {}},
+        "best": {"bubble": {}, "bubbleImproved": {}, "shell": {}},
+        "worst": {"bubble": {}, "bubbleImproved": {}, "shell": {}},
     }
 
-    for n in sizes:
-        print ("\nDATA SIZE: ", n)
+    for sizeTestArray in sizes:
+        print("\nDATA SIZE: ", sizeTestArray)
 
-        for gen_type in types:
-            print ("\n\tDATA TYPE:", gen_type)
+        for genType in typesArrayTest:
+            print("\n\tDATA TYPE:", genType)
 
-            data = genArr(n, gen_type)
+            testArray = genArr(sizeTestArray, genType)
 
-            data_bubble = np.copy(data)
-            bubble_op_count = bubbleSort(data_bubble)
-            print ("\tBubble sort operation count:", int(bubble_op_count))
-            data_plot[gen_type]['bubble'][n] = bubble_op_count
+            # Bubble sort test
+            testArrayBubble = testArray.copy()
+            operationCountBubble = bubbleSort(testArrayBubble)
+            print("\tBubble sort operation count:", int(operationCountBubble))
+            dataPlot[genType]["bubble"][sizeTestArray] = operationCountBubble
 
-            # Для тестування покращеного методу бульбашки
-            data_bubble_impr = np.copy(data)
-            bubble_impr_op_count = bubbleImprovedSort(data_bubble_impr)
-            print ("\tImproved bubble sort operation count:", int(bubble_impr_op_count))
-            data_plot[gen_type]['bubble_impr'][n] = bubble_impr_op_count
+            # Bubble sort improved
+            testArrayBubbleImproved = testArray.copy()
+            operationCountBubbleImproved = bubbleImprovedSort(testArrayBubbleImproved)
+            print(
+                "\tImproved bubble sort operation count:",
+                int(operationCountBubbleImproved),
+            )
+            dataPlot[genType]["bubbleImproved"][sizeTestArray] = (
+                operationCountBubbleImproved
+            )
 
-            data_insertion = np.copy(data)
-            #insertion_op_count = insertion_sort(data_insertion)
-            # print ("\tInsertion sort operation count:", int(insertion_op_count))
-            # data_plot[gen_type]['insertion'][n] = insertion_op_count
+            # Shell sort
+            testArrayShell = testArray.copy()
+            operationCountShell = shellSort(testArrayShell)
+            print("\tShell sort operation count:", int(operationCountShell))
+            dataPlot[genType]["shell"][sizeTestArray] = operationCountShell
 
-# plot_data(data_plot, logarithmic=True, oneplot=True)
+    plot_data(dataPlot, logarithmic=True, oneplot=True)
