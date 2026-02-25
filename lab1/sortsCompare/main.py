18d44d12751c518b9599b85f4e048f05fea97925
maximus <mkorobchenko21@gmail.com>
2026-02-25 21:58:44 +0200

    changed names of variables and functions; added coments

--- lab1/sortsCompare/main.py
+++ lab1/sortsCompare/main.py
@@ -4,8 +4,9 @@ import math
 # from plotData import plot_data
 
 
-def genArr(size: int, genType: str = "random") -> list:
-    match genType:
+def gen_arr(size: int, gen_type: str = "random") -> list:
+    # to generate an array for testing sorting algorithms
+    match gen_type:
         case "best":
             return [i for i in range(1, size + 1)]
         case "worst":
@@ -18,69 +19,98 @@ def genArr(size: int, genType: str = "random") -> list:
             return arr
 
 
+def sort_result(arr: list["classes.SortExperiment"]):
+    algo_order = {
+        "bubble": 0,
+        "bubbleImproved": 1,
+        "shell": 2,
+    }
+
+    return sorted(arr, key=lambda r: (r.size, algo_order[r.algorithm]))
+
+
 def print_result(results: list["classes.SortResult"]):
+    """
+    Prints formatted sorting statistics grouped by input size and algorithm.
+
+    The function expects a list of SortResult objects sorted by:
+    1) size
+    2) algorithm
+
+    example of print:
+        Size: 10
+            bubble sort:
+                    Type: random                    45
+                    Type: randomUniq                45
+                    Type: best                      45
+                    Type: worst                     45
+            bubbleImproved sort:
+                    Type: random                    44
+                    Type: randomUniq                42
+                    ...
+    """
+
     old_size = None
     old_algo = None
-    for r in results:
-        current_size = r.size
+    for obj in results:
+        current_size = obj.size
         if current_size != old_size:
             print(f"Size: {current_size}")
 
-        current_algo = r.algorithm
+        current_algo = obj.algorithm
         if current_algo != old_algo:
-            print(f"\t{r.algorithm} sort:")
+            print(f"\t{obj.algorithm} sort:")
 
-        print(f"\t\tType: {r.data_type:<10}\t{r.operations:>10,}")
+        print(f"\t\tType: {obj.data_type:<10}\t{obj.operations:>10,}")
 
         old_size = current_size
         old_algo = current_algo
 
 
-def bubbleSort(arr: list) -> int:
+def bubble_sort(arr: list) -> int:
     size = len(arr)
-    operationCount = 0
+    operation_count = 0
 
     for i in range(size - 1):
         for j in range(size - i - 1):
-            operationCount += 1
+            operation_count += 1
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-    return operationCount
+    return operation_count
 
 
-def bubbleImprovedSort(arr: list) -> int:
+def bubble_improved_sort(arr: list) -> int:
     size = len(arr)
-    operationCount = 0
+    operation_count = 0
 
     for i in range(size - 1):
         swapped = False
         for j in range(size - i - 1):
-            operationCount += 1
+            operation_count += 1
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 swapped = True
         if not swapped:
             break
 
-    return operationCount
+    return operation_count
 
 
-def shellSort(arr: list) -> int:  # using Tokuda gap
-    def getTokudaGaps(n: int) -> list:
+def shell_sort(arr: list) -> int:  # using Tokuda gap
+    def ge_tokuda_geps(n: int) -> list:
         gaps = []
         k = 1
-        while (
-            gap := math.ceil((9**k - 4**k) / (5 * 4 ** (k - 1)))
-        ) <= n:  # while this new counted gap <= n
+        # while this new counted gap <= n
+        while (gap := math.ceil((9**k - 4**k) / (5 * 4 ** (k - 1)))) <= n:
             gaps.append(gap)
             k += 1
 
         return reversed(gaps)
 
     size = len(arr)
-    operationCount = 0
-    gaps = getTokudaGaps(size)
+    operation_count = 0
+    gaps = ge_tokuda_geps(size)
 
     for gap in gaps:
         for i in range(gap, size):
@@ -88,16 +118,16 @@ def shellSort(arr: list) -> int:  # using Tokuda gap
             j = i
 
             while j >= gap and arr[j - gap] > temp:
-                operationCount += 1
+                operation_count += 1
                 arr[j] = arr[j - gap]
                 j -= gap
 
             if j >= gap:  # when in while loop was comparison, but it didnt counted
-                operationCount += 1
+                operation_count += 1
 
             arr[j] = temp
 
-    return operationCount
+    return operation_count
 
 
 if __name__ == "__main__":
@@ -106,9 +136,9 @@ if __name__ == "__main__":
     #
 
     algorithms = [
-        classes.SortAlgorithm(name="bubble", func=bubbleSort),
-        classes.SortAlgorithm(name="bubbleImproved", func=bubbleImprovedSort),
-        classes.SortAlgorithm(name="shell", func=shellSort),
+        classes.SortAlgorithm(name="bubble", func=bubble_sort),
+        classes.SortAlgorithm(name="bubbleImproved", func=bubble_improved_sort),
+        classes.SortAlgorithm(name="shell", func=shell_sort),
     ]
 
     Experiment = classes.SortExperiment(
@@ -119,17 +149,8 @@ if __name__ == "__main__":
 
     Experiment.run()
 
-    algo_order = {
-        "bubble": 0,
-        "bubbleImproved": 1,
-        "shell": 2,
-    }
-
-    sorted_results = sorted(
-        Experiment.results, key=lambda r: (r.size, algo_order[r.algorithm])
-    )
+    sorted_results = sort_result(Experiment.results)
 
     print_result(sorted_results)
 
     # plot_data(experiment.results)
-    # plot_data(dataPlot, logarithmic=True, oneplot=True)
