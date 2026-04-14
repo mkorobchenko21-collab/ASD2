132d8a486ebdc25ee7e6f33637d34267e7b5ff5c
maximus <mkorobchenko21@gmail.com>
2026-04-14 13:11:25 +0300

    docs: moved coment with task from main.py to README.md

--- lab2/invertion_merge/main.py
+++ lab2/invertion_merge/main.py
@@ -3,40 +3,6 @@ from pathlib import Path
 from math import inf
 
 
-"""
-from input file:
-    10 5
-    1 5 2 1 3 4
-    2 3 2 4 1 5
-    3 4 5 3 2 1
-    4 5 1 4 3 2
-    5 1 2 5 4 3
-    6 2 5 4 1 3
-    7 2 4 5 3 1
-    8 5 3 1 4 2
-    9 4 5 2 3 1
-    10 3 1 2 4 5
-
-where number of users U = 10 and number of films M = 5
-
-we need to get output file with:
-    6
-    3 3
-    2 4
-    7 4
-    9 4
-    5 5
-    1 7
-    4 7
-    8 7
-    10 8
-    6
-
-where user id for comparison is X = 6 (entered by user in termianl)
-output is sorted by second column (amount of inversions)
-"""
-
-
 def merge_and_count_split_inversions(arr: list, left: list, right: list):
     n1 = len(left)
     n2 = len(right)
